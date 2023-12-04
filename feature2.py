f0057639a7bb251e912beb45fdda9f5de61a3465
Kabiltan <58378166+Kabiltan@users.noreply.github.com>
2023-12-03 23:26:44 -0600

    Removed unused imports

--- feature2.py
+++ feature2.py
@@ -20,9 +20,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import unquote
 from html import unescape
 from selenium import webdriver
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
